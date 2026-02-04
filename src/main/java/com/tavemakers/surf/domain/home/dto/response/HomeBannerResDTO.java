cd3f325651a01e44143c260a8c1114e737c1f782
tjdgus3488 <tjdgus3488@naver.com>
2026-02-04 20:21:23 +0900

    feat: HomeBannerResDTO name, status 추가

--- src/main/java/com/tavemakers/surf/domain/home/dto/response/HomeBannerResDTO.java
+++ src/main/java/com/tavemakers/surf/domain/home/dto/response/HomeBannerResDTO.java
@@ -10,6 +10,9 @@ public record HomeBannerResDTO(
         @Schema(description = "배너 ID", example = "1")
         Long id,
 
+        @Schema(description = "배너 이름", example = "새로운 17기 환영 배너")
+        String name,
+
         @Schema(description = "배너 이미지 URL", example = "https://example.com/banner1.jpg")
         String imageUrl,
 
@@ -17,14 +20,19 @@ public record HomeBannerResDTO(
         String linkUrl,
 
         @Schema(description = "배너 노출 순서", example = "1")
-        Integer displayOrder
+        Integer displayOrder,
+
+        @Schema(description = "배너 활성 상태", example = "true")
+        boolean status
 ) {
     public static HomeBannerResDTO from(HomeBanner b) {
         return HomeBannerResDTO.builder()
                 .id(b.getId())
+                .name(b.getName())
                 .imageUrl(b.getImageUrl())
                 .linkUrl(b.getLinkUrl())
                 .displayOrder(b.getDisplayOrder())
+                .status(b.isStatus())
                 .build();
     }
 }
