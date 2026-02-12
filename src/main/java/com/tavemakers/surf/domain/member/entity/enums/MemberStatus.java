80e37f5f4cf53bf9d19870ea2aa459ef84f6759c
koreaioi <koreaioi@gachon.ac.kr>
2026-02-12 18:07:50 +0900

    feat: List<String>을 List<MemberStatus>로 변경하는 enum 로직 추가

--- src/main/java/com/tavemakers/surf/domain/member/entity/enums/MemberStatus.java
+++ src/main/java/com/tavemakers/surf/domain/member/entity/enums/MemberStatus.java
@@ -1,9 +1,19 @@
 package com.tavemakers.surf.domain.member.entity.enums;
 
+import java.util.List;
+
 public enum MemberStatus {
     REGISTERING, // 가입중
     WAITING,     // 대기중
     APPROVED,    // 승인
     REJECTED,    // 거절됨
     WITHDRAWN    // 탈퇴됨
+    ;
+
+    public static List<MemberStatus> valueOf(List<String> rawMemberStatuses) {
+        return rawMemberStatuses.stream()
+                .map(MemberStatus::valueOf)
+                .toList();
+    }
+
 }
