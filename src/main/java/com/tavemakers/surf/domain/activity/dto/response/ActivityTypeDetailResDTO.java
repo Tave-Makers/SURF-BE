189cac7571b4a18061c9f9011229f087b190eaa3
koreaioi <koreaioi@gachon.ac.kr>
2026-02-21 23:07:14 +0900

    feat: ActivityTypeDetailResDTO 추가

---
+++ src/main/java/com/tavemakers/surf/domain/activity/dto/response/ActivityTypeDetailResDTO.java
@@ -0,0 +1,25 @@
+package com.tavemakers.surf.domain.activity.dto.response;
+
+import com.tavemakers.surf.domain.activity.entity.enums.ActivityType;
+import com.tavemakers.surf.domain.activity.entity.enums.AppliedTarget;
+import com.tavemakers.surf.domain.activity.entity.enums.ScoreType;
+import lombok.Builder;
+
+@Builder
+public record ActivityTypeDetailResDTO(
+        String displayName,
+        Integer delta,
+        ScoreType scoreType,
+        AppliedTarget appliedTarget,
+        String category
+) {
+    public static ActivityTypeDetailResDTO of(ActivityType activityType) {
+        return ActivityTypeDetailResDTO.builder()
+                .displayName(activityType.getDisplayName())
+                .delta(activityType.getDelta())
+                .scoreType(activityType.getScoreType())
+                .appliedTarget(activityType.getAppliedTarget())
+                .category(activityType.getCategory().getDisplayName())
+                .build();
+    }
+}
