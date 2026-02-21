3632d65529042d25485503eb07a512322081863f
koreaioi <koreaioi@gachon.ac.kr>
2026-02-21 23:31:23 +0900

    feat: PersonalActivityScore에 상점 벌점 누적합 필드 추가

--- src/main/java/com/tavemakers/surf/domain/score/entity/PersonalActivityScore.java
+++ src/main/java/com/tavemakers/surf/domain/score/entity/PersonalActivityScore.java
@@ -1,5 +1,6 @@
 package com.tavemakers.surf.domain.score.entity;
 
+import com.tavemakers.surf.domain.activity.entity.enums.ActivityType;
 import com.tavemakers.surf.domain.member.entity.Member;
 import com.tavemakers.surf.domain.team.entity.Team;
 import com.tavemakers.surf.global.common.entity.BaseEntity;
@@ -30,6 +31,12 @@ public class PersonalActivityScore extends BaseEntity {
     private Team team;
 
     @Column(precision = 19, scale = 1)
+    private BigDecimal rewardPrefixSum = BigDecimal.ZERO.setScale(1, RoundingMode.HALF_UP);;
+
+    @Column(precision = 19, scale = 1)
+    private BigDecimal penaltyPrefixSum = BigDecimal.ZERO.setScale(1, RoundingMode.HALF_UP);;
+
+    @Column(precision = 19, scale = 1)
     private BigDecimal score = BigDecimal.ZERO.setScale(1, RoundingMode.HALF_UP);
 
     public BigDecimal updateScore(BigDecimal score) {
@@ -37,10 +44,26 @@ public class PersonalActivityScore extends BaseEntity {
         return this.score;
     }
 
+    // TODO 상점 누적합, 벌점 누적합
+    public BigDecimal updateScore(ActivityType activityType) {
+        BigDecimal delta = BigDecimal.valueOf(activityType.getDelta());
+        this.score = this.score.add(delta);
+
+        if (activityType.isReward()) {
+            this.rewardPrefixSum = this.rewardPrefixSum.add(delta);
+            return this.score;
+        }
+
+        this.penaltyPrefixSum = this.penaltyPrefixSum.add(delta);
+        return this.score;
+    }
+
     public static PersonalActivityScore from(Member member) {
         return PersonalActivityScore.builder()
                 .member(member)
                 .score(member.isYB() ? BigDecimal.valueOf(100) : BigDecimal.valueOf(50)) // 기본 점수 100
+                .rewardPrefixSum(BigDecimal.valueOf(0))
+                .penaltyPrefixSum(BigDecimal.valueOf(0))
                 .build();
     }
 
@@ -48,6 +71,8 @@ public class PersonalActivityScore extends BaseEntity {
         return PersonalActivityScore.builder()
                 .team(team)
                 .score(BigDecimal.valueOf(0))
+                .rewardPrefixSum(BigDecimal.valueOf(0))
+                .penaltyPrefixSum(BigDecimal.valueOf(0))
                 .build();
     }
 
