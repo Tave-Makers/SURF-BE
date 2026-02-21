e7e59664584b3d34faff6349d656c85396895cb0
koreaioi <koreaioi@gachon.ac.kr>
2026-02-21 23:06:19 +0900

    feat: ActivityCategory에 displayName 추가

--- src/main/java/com/tavemakers/surf/domain/activity/entity/enums/ActivityCategory.java
+++ src/main/java/com/tavemakers/surf/domain/activity/entity/enums/ActivityCategory.java
@@ -6,11 +6,14 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum ActivityCategory {
-    STUDY_ON_PERSONAL,
-    PROJECT_ON_PERSONAL,
-    STUDY_ON_TEAM,
-    PROJECT_ON_TEAM,
-    REGULAR_SESSION,
-    ACTIVITY,
+    STUDY_ON_PERSONAL("스터디(개인)"),
+    PROJECT_ON_PERSONAL("프로젝트(개인)"),
+    STUDY_ON_TEAM("스터디(팀)"),
+    PROJECT_ON_TEAM("프로젝트(팀)"),
+    REGULAR_SESSION("정규 행사"),
+    ACTIVITY("활동"),
     ;
+
+    final String displayName;
+
 }
