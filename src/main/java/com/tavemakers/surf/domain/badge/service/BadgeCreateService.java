337729c5bc089d81e6bb8bfcd1ef29be586fed14
LAICME <ehxhfks@naver.com>
2026-02-24 23:05:04 +0900

    feat: 배지 생성 service 구현 #271

---
+++ src/main/java/com/tavemakers/surf/domain/badge/service/BadgeCreateService.java
@@ -0,0 +1,33 @@
+package com.tavemakers.surf.domain.badge.service;
+
+import com.tavemakers.surf.domain.badge.dto.request.BadgeCreateReqDTO;
+import com.tavemakers.surf.domain.badge.entity.Badge;
+import com.tavemakers.surf.domain.badge.repository.BadgeRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class BadgeCreateService {
+
+    private final BadgeRepository badgeRepository;
+
+    /** 배지 생성 */
+    @Transactional
+    public Long create(BadgeCreateReqDTO request) {
+
+        // 요청 DTO 값을 기반으로 배지 엔티티 생성
+        Badge badge = new Badge(
+                request.getName(),
+                request.getImageUrl(),
+                request.getDescription(),
+                request.getRequirement()
+        );
+
+        // 배지 저장
+        badgeRepository.save(badge);
+
+        return badge.getId();
+    }
+}
