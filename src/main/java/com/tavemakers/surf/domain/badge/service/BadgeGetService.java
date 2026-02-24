ce1457699bef412a515a069de8572307e8ddd9a4
LAICME <ehxhfks@naver.com>
2026-02-24 23:07:04 +0900

    feat: 배지 목록 조회 service 구현 #271

---
+++ src/main/java/com/tavemakers/surf/domain/badge/service/BadgeGetService.java
@@ -0,0 +1,24 @@
+package com.tavemakers.surf.domain.badge.service;
+
+import com.tavemakers.surf.domain.badge.entity.Badge;
+import com.tavemakers.surf.domain.badge.repository.BadgeRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Slice;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class BadgeGetService {
+
+    private final BadgeRepository badgeRepository;
+
+    /** 배지 목록 조회 (무한스크롤) */
+    @Transactional(readOnly = true)
+    public Slice<Badge> getBadgeList(int page) {
+        return badgeRepository.findAllBy(
+                PageRequest.of(page, 20)
+        );
+    }
+}
