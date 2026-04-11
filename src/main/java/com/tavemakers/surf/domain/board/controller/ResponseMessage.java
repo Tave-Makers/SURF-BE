2a23e8dac1040105935de7eb10be473a96aed76d
zldzldzz <wjl100735@gmail.com>
2026-04-11 16:16:48 +0900

    feat: ResponseMessage 추가 #292

--- src/main/java/com/tavemakers/surf/domain/board/controller/ResponseMessage.java
+++ src/main/java/com/tavemakers/surf/domain/board/controller/ResponseMessage.java
@@ -10,7 +10,9 @@ public enum ResponseMessage {
     BOARD_CREATED("[게시판]이 성공적으로 생성되었습니다."),
     BOARD_UPDATED("[게시판]이 성공적으로 수정되었습니다."),
     BOARD_DELETED("[게시판]이 성공적으로 삭제되었습니다."),
-    BOARD_READ("[게시판]이 성공적으로 조회되었습니다.");
+    BOARD_READ("[게시판]이 성공적으로 조회되었습니다."),
+
+    CATEGORY_CREATED("[카테고리]가 성공적으로 생성되었습니다.");
 
     private final String message;
 
