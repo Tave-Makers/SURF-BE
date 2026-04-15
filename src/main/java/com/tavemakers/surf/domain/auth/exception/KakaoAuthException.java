4c61f4d5e8f7190294cf838ff0a3d407f84e86a0
goodjunseon <pzs2001926@gmail.com>
2026-04-15 17:33:48 +0900

    fix: KakaoAuthException을 BaseException 상속으로 변경하여 HTTP 상태코드 유실 방지

--- src/main/java/com/tavemakers/surf/domain/auth/exception/KakaoAuthException.java
+++ src/main/java/com/tavemakers/surf/domain/auth/exception/KakaoAuthException.java
@@ -1,9 +1,11 @@
 package com.tavemakers.surf.domain.auth.exception;
 
+import com.tavemakers.surf.global.common.exception.BaseException;
 import org.springframework.http.HttpStatus;
 
-public class KakaoAuthException extends RuntimeException {
-  public KakaoAuthException(HttpStatus status, String message) {
-    super(message);
-  }
+/** 카카오 OAuth API 호출 실패 시 발생하는 예외 */
+public class KakaoAuthException extends BaseException {
+    public KakaoAuthException(HttpStatus status, String message) {
+        super(status, message);
+    }
 }
