b1822fe6ed79b825dcfd84fc4bc628b3eea76473
zldzldzz <wjl100735@gmail.com>
2026-04-26 17:15:23 +0900

    refactor: 공지사항 권한 예외 주석 추가
    - BoardWriteNotAllowedException 주석 추가

--- src/main/java/com/tavemakers/surf/domain/post/exception/BoardWriteNotAllowedException.java
+++ src/main/java/com/tavemakers/surf/domain/post/exception/BoardWriteNotAllowedException.java
@@ -5,6 +5,7 @@ import com.tavemakers.surf.global.common.exception.BaseException;
 import static com.tavemakers.surf.domain.post.exception.ErrorMessage.BOARD_WRITE_NOT_ALLOWED;
 
 public class BoardWriteNotAllowedException extends BaseException {
+    /** 공지사항 게시글 작성 권한이 없을 때 발생하는 예외 */
     public BoardWriteNotAllowedException() {
         super(BOARD_WRITE_NOT_ALLOWED.getStatus(), BOARD_WRITE_NOT_ALLOWED.getMessage());
     }
