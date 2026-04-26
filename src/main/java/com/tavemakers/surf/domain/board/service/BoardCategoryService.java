cd42652ba85a314c0d0e9914eff03a79f1911559
zldzldzz <wjl100735@gmail.com>
2026-04-26 17:53:41 +0900

    refactor: BoardCategory의 유니크 검증 추가
    - createBoardCategory의 유니크 검증 시도
    - BoardCategory의 유니크 제약 위반시 500이 아닌 409로 전달 되도록 수정

--- src/main/java/com/tavemakers/surf/domain/board/service/BoardCategoryService.java
+++ src/main/java/com/tavemakers/surf/domain/board/service/BoardCategoryService.java
@@ -4,9 +4,11 @@ import com.tavemakers.surf.domain.board.dto.request.BoardCategoryCreateReqDTO;
 import com.tavemakers.surf.domain.board.dto.response.BoardCategoryResDTO;
 import com.tavemakers.surf.domain.board.entity.Board;
 import com.tavemakers.surf.domain.board.entity.BoardCategory;
+import com.tavemakers.surf.domain.board.exception.BoardCategoryAlreadyExistsException;
 import com.tavemakers.surf.domain.board.repository.BoardCategoryRepository;
 import com.tavemakers.surf.global.logging.LogEvent;
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -20,8 +22,17 @@ public class BoardCategoryService {
     @Transactional
     @LogEvent(value = "board.category.create", message = "게시판 카테고리 생성 성공")
     public BoardCategoryResDTO createBoardCategory(Board board, BoardCategoryCreateReqDTO req) {
+        if (boardCategoryRepository.existsByBoardAndSlug(board, req.slug())) {
+            throw new BoardCategoryAlreadyExistsException();
+        }
+
         BoardCategory boardCategory = BoardCategory.of(board, req);
-        BoardCategory saved = boardCategoryRepository.save(boardCategory);
-        return BoardCategoryResDTO.from(saved);
+
+        try {
+            BoardCategory saved = boardCategoryRepository.saveAndFlush(boardCategory);
+            return BoardCategoryResDTO.from(saved);
+        } catch (DataIntegrityViolationException e) {
+            throw new BoardCategoryAlreadyExistsException();
+        }
     }
 }
