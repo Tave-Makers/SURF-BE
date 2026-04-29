aaa52d3130b62cf39a8b8307f37e8400b1b46fb1
LAICME <ehxhfks@naver.com>
2026-04-30 01:13:49 +0900

    refact: BadgeDetailResDTO record 포맷으로 통일 #303

--- src/main/java/com/tavemakers/surf/domain/badge/dto/response/BadgeDetailResDTO.java
+++ src/main/java/com/tavemakers/surf/domain/badge/dto/response/BadgeDetailResDTO.java
@@ -2,25 +2,22 @@ package com.tavemakers.surf.domain.badge.dto.response;
 
 import com.tavemakers.surf.domain.badge.entity.Badge;
 import lombok.Builder;
-import lombok.Getter;
 
-@Getter
 @Builder
-public class BadgeDetailResDTO {
-
-    private Long badgeId;
-    private String name;
-    private String imageUrl;
-    private String description;
-    private String requirement;
-
+public record BadgeDetailResDTO(
+        Long badgeId,
+        String name,
+        String imageUrl,
+        String description,
+        String requirement
+) {
     public static BadgeDetailResDTO from(Badge badge) {
         return BadgeDetailResDTO.builder()
-                .badgeId(badge.getId())
-                .name(badge.getName())
-                .imageUrl(badge.getImageUrl())
-                .description(badge.getDescription())
-                .requirement(badge.getRequirement())
-                .build();
+                        .badgeId(badge.getId())
+                        .name(badge.getName())
+                        .imageUrl(badge.getImageUrl())
+                        .description(badge.getDescription())
+                        .requirement(badge.getRequirement())
+                        .build();
     }
 }
